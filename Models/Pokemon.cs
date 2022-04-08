dc5e5f24e7d5cc23db92fbfe1f937da35a4e89cc
AllanRichard <allanrichard_silva@hotmail.com>
2022-04-07 21:27:57 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Pokemon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioPokeApi.Models
+{
+    internal class Pokemon
+    {
+        public string Name { get; set; }
+        public string[] Types { get; set; }
+        public string[] Abilities { get; set; }
+        public int Weight { get; set; }
+        public int Height { get; set; }
+        public string[] Sprites { get; set; }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Types: {Types}, Abilities: {Abilities}, Weight: {Weight}, Height: {Height}, Sprites: {Sprites}";
+        }
+    }
+
+    internal class PokemonList
+    {
+        public Pokemon[]? Pokemons { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Array de pokemons
+string[] pokemons = { "Charmander", "Squirtle", "Caterpie", "Weedle", "Pidgey", "Pidgeotto", "Rattata", "Spearow", "Fearow", "Arbok", "Pikachu", "Sandshrew" };
+
+foreach (var pokemon in pokemons)
+{
+    var name = pokemon.ToLower();
+    using var client = new HttpClient();
+    var httpResponseMessage = await client.GetAsync("https://pokeapi.co/api/v2/pokemon/" + name);
+    if (httpResponseMessage.IsSuccessStatusCode)
+    {
+        var content = await httpResponseMessage.Content.ReadAsStringAsync();
+        Console.WriteLine(content);
+    }
+}
